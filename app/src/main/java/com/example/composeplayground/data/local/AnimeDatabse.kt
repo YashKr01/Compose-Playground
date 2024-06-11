031cc804334ee7e7a088f29a5070fc6bc96e7ae6
yash <yash.kumar@grofers.com>
2024-06-11 22:40:28 +0530

    remote key dao setup

--- app/src/main/java/com/example/composeplayground/data/local/AnimeDatabse.kt
+++ app/src/main/java/com/example/composeplayground/data/local/AnimeDatabse.kt
@@ -3,11 +3,14 @@ package com.example.composeplayground.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.composeplayground.data.local.dao.HeroDao
+import com.example.composeplayground.data.local.dao.HeroRemoteKeyDao
 import com.example.composeplayground.domain.model.Hero
+import com.example.composeplayground.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class AnimeDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 
 }
