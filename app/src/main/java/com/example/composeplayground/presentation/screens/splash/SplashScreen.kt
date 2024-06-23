996f8931106a3f123fda0df2d806a2d0713e5b8a
yash <yash.kumar@grofers.com>
2024-06-23 11:37:30 +0530

    pushed uncommitted files

---
+++ app/src/main/java/com/example/composeplayground/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,44 @@
+package com.example.composeplayground.presentation.screens.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.example.composeplayground.R
+import com.example.composeplayground.ui.theme.Purple500
+import com.example.composeplayground.ui.theme.Purple700
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    Splash()
+}
+
+
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier
+            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = stringResource(R.string.app_logo)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun SplashScreenPreview(modifier: Modifier = Modifier) {
+    Splash()
+}
