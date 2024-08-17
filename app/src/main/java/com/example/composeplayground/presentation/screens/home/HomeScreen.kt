09bf80be878c2b5d94020f8b7c29cdb21a63d288
yash <yash.kumar@grofers.com>
2024-08-17 13:51:08 +0530

    Rating widget

--- app/src/main/java/com/example/composeplayground/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composeplayground/presentation/screens/home/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.example.composeplayground.presentation.screens.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.composeplayground.presentation.components.RatingWidget
+import com.example.composeplayground.ui.theme.LARGE_PADDING
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel()
@@ -14,7 +19,7 @@ fun HomeScreen(
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
 
     Scaffold(
-        topBar = { HomeTopBar(onSearchClicked = {}) }
+        topBar = { HomeTopBar(onSearchClicked = {}) },
     ) {
 
     }
